71f5beb1c398cc8ee554cc73b934dfaa1025c573
Jay Janodia <jayjanodia@Jays-MBP.attlocal.net>
2021-12-16 15:01:34 -0500

    Completed Assignment 1

--- 01-starting-setup/src/components/ExpenseItem.js
+++ 01-starting-setup/src/components/ExpenseItem.js
@@ -1,12 +1,16 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
-function ExpenseItem() {
+function ExpenseItem(props) {
+    /*const expenseDate = new Date(2021, 2, 28);
+    const expenseTitle = 'Car Insurance';
+    const expenseAmount = 294.67;*/
     return (
     <div className="expense-item">
-        <div>March 28, 2018</div>
+        <ExpenseDate date={props.date}/>
         <div className="expense-item__description">
-            <h2>Car Insurance</h2>
-            <div className="expense-item__price">$294.67</div>
+            <h2>{props.name}</h2>
+            <div className="expense-item__price">${props.amount}</div>
         </div>
     </div>
     );
